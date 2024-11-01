a6ef387997e7d12e1a8b9d499484ce5704f56b1e
Vorapon-Pon <vorapon.w.pon@gmail.com>
2024-11-02 03:59:31 +0700

    .

--- numer-project-backend/index.js
+++ numer-project-backend/index.js
@@ -7,9 +7,7 @@ const swaggerUi = require('swagger-ui-express');
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("/", async (req, res) => {
-  res.send("Server is running");
-});
+
 
 //swagger
 const swaggerOptions = {
